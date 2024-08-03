de76524e7a97d6f74e90f6662ad7b2628aacbd37
Chelsea Jarvis <chelseajarvis301@icloud.com>
2024-08-02 19:49:36 -0600

    added base type.Defs

--- schemas/type.Defs.js
+++ schemas/type.Defs.js
@@ -1,1 +1,29 @@
-﻿
+﻿const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+  type User {
+   
+  }
+
+  type Book {
+   
+  }
+
+  type Auth {
+ 
+  }
+
+  type Query {
+
+  }
+
+  input BookInput {
+   
+  }
+
+  type Mutation {
+
+  }
+`;
+
+module.exports = typeDefs;
