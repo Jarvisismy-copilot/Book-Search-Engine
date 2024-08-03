919fcaa60bc3f61a0d49168f05799bac9240b075
Chelsea Jarvis <chelseajarvis301@icloud.com>
2024-08-02 19:32:16 -0600

    auth.js set ip/ index.js schema set up exports

--- auth.js
+++ auth.js
@@ -1,1 +1,17 @@
-﻿
+﻿const jwt = require('jsonwebtoken');
+const { AuthenticationError } = require('apollo-server-express');
+
+module.exports = (req, res, next) => {
+  const token = req.headers.authorization || '';
+
+  if (token) {
+    try {
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      req.user = decoded;
+    } catch (err) {
+      throw new AuthenticationError('Invalid or expired token');
+    }
+  }
+
+  next();
+};
